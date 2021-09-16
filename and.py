8e5a150936f7aead995fa2e623d20c3128c33e5d
Dhyey Sherasia <dhyeysherasia2002@gmail.com>
2021-09-16 16:51:51 +0530

    modified and.py

--- and.py
+++ and.py
@@ -1,23 +1,32 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_model
 import pandas as pd
 
-AND = {
-    "x1": [0, 0, 1, 1],
-    "x2": [0, 1, 0, 1],
-    "y": [0, 0, 0, 1]
-}
 
-df = pd.DataFrame(AND)
-df
+def main(data, eta, epochs, filename):
 
-x, y = prepare_data(df)
+    df = pd.DataFrame(data)
+    df
 
-LEARNING_RATE = 0.3
-EPOCHS = 10
+    x, y = prepare_data(df)
 
-model = Perceptron(eta=LEARNING_RATE, epochs=EPOCHS)  # Will initialize the random weights
-model.fit(x, y)
+    model = Perceptron(eta=eta, epochs=epochs)  # Will initialize the random weights
+    model.fit(x, y)
+
+    save_model(model, filename)
 
 
 
+
+if __name__ == '__main__':  # --> Entry point for this file
+
+    LEARNING_RATE = 0.3
+    EPOCHS = 10
+
+    AND = {
+            "x1": [0, 0, 1, 1],
+            "x2": [0, 1, 0, 1],
+            "y": [0, 0, 0, 1]
+        }
+
+    main(data=AND, eta=LEARNING_RATE, epochs=EPOCHS, filename="and.model")
