86390521fbe2c724b3b9555fff846355687c559a
Dhyey Sherasia <dhyeysherasia2002@gmail.com>
2021-10-03 11:47:56 +0530

    docstring updated

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -3,12 +3,26 @@ import os
 import joblib
 
 def prepare_data(df):
+    """Prepares the data for training
+
+    Args:
+        df (pandas dataframe): data for training and testing the model
+
+    Returns:
+        tuple: returns splitted data into features and target
+    """
     x = df.drop("y", axis=1)
     y = df["y"]
 
     return x, y
 
 def save_model(model, filename):
+    """Saves the trained model in the given
+
+    Args:
+        model (python object): the trained model
+        filename (str): path to save the trained model
+    """
     model_dir = "models"
 
     try:
